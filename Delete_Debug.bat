b3396fe66176b9f6bd286e45c5ec06eb44c56b17
Konrad Mueller <i15019@hb.dhbw-stuttgart.de>
2016-10-19 13:22:00 +0200

    Updated Xamarin.Forms.

---
+++ Delete_Debug.bat
@@ -0,0 +1,10 @@
+ECHO OFF
+rd /s /q "%~dp0\PartyUp\PartyUp\bin\Debug"
+rd /s /q "%~dp0\PartyUp\PartyUp\obj\Debug"
+rd /s /q "%~dp0\PartyUp\PartyUp\bin\Release"
+rd /s /q "%~dp0\PartyUp\PartyUp\obj\Release"
+rd /s /q "%~dp0\PartyUp\PartyUp.Droid\bin\Debug"
+rd /s /q "%~dp0\PartyUp\PartyUp.Droid\obj\Debug"
+rd /s /q "%~dp0\PartyUp\PartyUp.Droid\bin\Release"
+rd /s /q "%~dp0\PartyUp\PartyUp.Droid\obj\Release"
+PAUSE
