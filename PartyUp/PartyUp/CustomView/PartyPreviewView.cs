90a1528ea4d01d4838ae917f3490a198c9803a38
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-10-24 19:02:11 +0200

    Modify PartyPreviewPage

--- PartyUp/PartyUp/CustomView/PartyPreviewView.cs
+++ PartyUp/PartyUp/CustomView/PartyPreviewView.cs
@@ -11,55 +11,83 @@ namespace PartyUp.CustomView
     public class PartyPreviewView : PreviewView
     {
         private readonly Party _party;
-
-      
+        private readonly IUserLocationService _userLocationService = Xamarin.Forms.DependencyService.Get<IUserLocationService>();
+        private readonly TapGestureRecognizer _closeTapGestureRecognizer = new TapGestureRecognizer();
 
         public PartyPreviewView(Party party, double parentHeight, double parentWidth)
-        { 
+        {
             BackgroundColor = Color.White;
-             
             _party = party;
             //Title label
             var titleLabel = new Label {Text = party.Name};
-
             //Close label
             var closeLabel = new Label {Text = "Close", HorizontalOptions = LayoutOptions.Start};
-            TapGestureRecognizer closeTapGestureRecognizer = new TapGestureRecognizer();
-            closeLabel.GestureRecognizers.Add(closeTapGestureRecognizer);
-            closeTapGestureRecognizer.Tapped += CloseTapGestureRecognizerOnTapped;
-            var bla = Xamarin.Forms.DependencyService.Get<IUserLocationService>();
-            Coordinates s = bla.GetUserCoordinates();
-            //TODO Set coordinates, customize the map
+            //Set TabGesture
+            closeLabel.GestureRecognizers.Add(_closeTapGestureRecognizer);
+            _closeTapGestureRecognizer.Tapped += CloseTapGestureRecognizerOnTapped;
+            // TODO Shows the current Position
+            Coordinates userCoordinates = _userLocationService.GetUserCoordinates();
             Map map = new Map(MapSpan.FromCenterAndRadius(
-                new Position((double)s.Latitude, (double)s.Longitude), Distance.FromMiles(0.3)))
+                new Position(userCoordinates.Latitude, userCoordinates.Longitude),
+                Distance.FromMiles(0.3)))
             {
                 IsShowingUser = true
             };
 
-            Grid layoutGrid = new Grid
+            var layoutGrid = new Grid
             {
                 RowDefinitions =
                 {
-                    new RowDefinition { Height = new GridLength(40, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(parentWidth / 2, GridUnitType.Absolute) }
+                    new RowDefinition {Height = new GridLength(40, GridUnitType.Absolute)},
+                    new RowDefinition {Height = new GridLength(parentWidth/2, GridUnitType.Absolute)}
                 },
                 Children =
                 {
                     titleLabel,
                     closeLabel,
-                    {map, 0, 1 }
+                    {map, 0, 1}
                 }
             };
-            Content = layoutGrid;
-
-            //TODO Add more party information
+            //TODO Implments informations
+            var stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    layoutGrid,
+                    new Grid
+                    {
+                        ColumnDefinitions = new ColumnDefinitionCollection
+                        {
+                            new ColumnDefinition {Width = new GridLength(50, GridUnitType.Star)},
+                            new ColumnDefinition {Width = new GridLength(50, GridUnitType.Star)},
+                        },
+                        Children =
+                        {
+                            {new Label
+                            {
+                                Text = "Name",
+                                HorizontalOptions = LayoutOptions.Start,
+                            },0,0},
+                            {new Label
+                            {
+                                Text = _party.Name,
+                                HorizontalOptions = LayoutOptions.End,
+                            },1,0}
+                        }
+                    }
+                }
+            };
 
             HeightRequest = layoutGrid.RowDefinitions.Sum(o => o.Height.Value);
+            Content = new ScrollView
+            {
+                Content = stackLayout
+            };
         }
 
         private void CloseTapGestureRecognizerOnTapped(object sender, EventArgs eventArgs)
         {
             CloseView();
-        } 
+        }
     }
 }
