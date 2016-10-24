573f4db6eaa33e2604afe302e9c84e82b1a64446
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-10-24 16:57:47 +0200

    Delete TestPage and Add ListView.

---
+++ PartyUp/PartyUp/CustomView/ListView.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+
+namespace PartyUp.CustomView
+{
+    public class ListView : Grid
+    {
+
+        public ListView()
+        {
+            ColumnDefinitions = new ColumnDefinitionCollection
+            {
+                new ColumnDefinition {Width = new GridLength(1,GridUnitType.Auto )},
+                new ColumnDefinition {Width = new GridLength(1,GridUnitType.Auto )},
+                new ColumnDefinition {Width = new GridLength(1,GridUnitType.Auto )}
+            };
+            RowDefinitions = new RowDefinitionCollection
+            {
+                new RowDefinition {Height = new GridLength(1, GridUnitType.Absolute)},
+                new RowDefinition {Height = new GridLength(1, GridUnitType.Auto)},
+                new RowDefinition {Height = new GridLength(1, GridUnitType.Absolute)},
+            };
+            Children.Add(new BoxView
+            {
+                HeightRequest = 1
+            },0,0);
+            Children.Add(new BoxView
+            {
+                HeightRequest = 1
+            },2,0);
+            Children.Add(new Label
+            {
+                Text = "Bla"
+            },1,1);
+            
+        }
+    }
+}
--- PartyUp/PartyUp/View/HistoryPage.cs
+++ PartyUp/PartyUp/View/HistoryPage.cs
@@ -1,10 +1,29 @@
 ﻿using MvvmNano.Forms;
 using PartyUp.ViewModel;
+using Xamarin.Forms;
 
 namespace PartyUp.View
 {
     public class HistoryPage : MvvmNanoContentPage<HistoryViewModel>
     {
-
+        public HistoryPage()
+        {
+            var stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    new CustomView.ListView(),
+                    new CustomView.ListView(),
+                    new CustomView.ListView(),
+                    new CustomView.ListView(),
+                }
+            };
+            Title = "History";
+            Content = new ScrollView
+            {
+                Content = stackLayout,
+                Orientation = ScrollOrientation.Vertical
+            };
+        }
     }
 }
