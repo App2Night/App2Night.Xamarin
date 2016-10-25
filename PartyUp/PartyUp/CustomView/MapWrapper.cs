d5520f8eefd4b5e0a4481fafd11749e83b032f18
Konrad Mueller <i15019@hb.dhbw-stuttgart.de>
2016-10-25 19:01:24 +0200

    Added a map wrapper to avoid GMapps exception.

---
+++ PartyUp/PartyUp/CustomView/MapWrapper.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace PartyUp.CustomView
+{
+    public class MapWrapper : ContentView
+    {
+        private readonly Map _map;
+
+        public MapWrapper(Map map)
+        {
+            _map = map;
+            if (Device.OS == TargetPlatform.Android && !App.MapAvailable)
+            {
+                BackgroundColor = Color.Gray;
+                Content = new Label {Text = "Google maps is not installed on this device."};
+            }
+            else
+            {
+                Content = map;
+            }
+        }
+
+        public Map Map => _map;
+    }
+}
