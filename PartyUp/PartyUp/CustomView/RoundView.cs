0c4753b634911143ccfcbb63f63a3e3d11dc6aa6
FriedSloth <k.l.mueller@outlook.com>
2016-10-30 15:55:35 +0100

    Added round view without flat bottom, added edge.

--- PartyUp/PartyUp/CustomView/RoundView.cs
+++ PartyUp/PartyUp/CustomView/RoundView.cs
@@ -6,5 +6,8 @@ namespace PartyUp.CustomView
     public class RoundView : ContentView
     {
         public bool FlatBottom { get; set; }= false;
+        public bool Edge { get; set; } = true;
+        public double EdgeSize { get; set; } = 10;
+        public Color EdgeColor { get; set; } = Color.Accent;
     }
 }
