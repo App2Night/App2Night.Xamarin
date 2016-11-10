ab40ecc2164367a1d5f4b0cdd7ba6878eab84419
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-11-10 18:16:00 +0100

    * ILoadImage.cs: * AndroidLoadImage.cs:
    * Resource.Designer.cs: Modify ILoadImage.

---
+++ App2Night/App2Night.Droid/Service/AndroidLoadImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AndroidLoadImage))]
+namespace App2Night.Droid
+{
+	public class AndroidLoadImage : ILoadImage
+	{
+		public Image ILoadImage()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- App2Night/App2Night/DependencyService/ILoadImage.cs
+++ App2Night/App2Night/DependencyService/ILoadImage.cs
@@ -3,14 +3,11 @@ using Xamarin.Forms;
 
 namespace App2Night
 {
-	public class ILoadImage
+	public interface ILoadImage
 	{
-		public ILoadImage()
-		{
 			/// <summary>
 			/// Returns an image from local device.
 			/// </summary>
-			Image ILoadImage;
-		}
+			Image ILoadImage();
 	}
 }
