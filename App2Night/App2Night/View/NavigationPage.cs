456fafa8d3b74775b671e10ce111824cfc8cd878
Konrad Mueller <i15019@hb.dhbw-stuttgart.de>
2016-11-17 14:38:28 +0100

    Modified RowHeight.

--- App2Night/App2Night/View/NavigationPage.cs
+++ App2Night/App2Night/View/NavigationPage.cs
@@ -1,4 +1,5 @@
-﻿using App2Night.CustomView.Template;
+﻿using System.Linq;
+using App2Night.CustomView.Template;
 using App2Night.CustomView.View;
 using App2Night.ViewModel;
 using MvvmNano.Forms;
@@ -8,37 +9,43 @@ namespace App2Night.View
 {
     public class NavigationPage : MvvmNanoMasterDetailPage<NavigationViewModel>
     {
+        Grid _layoutGrid;
+
         public NavigationPage()
         {
             if(Device.OS == TargetPlatform.iOS) Padding = new Thickness(0, 20, 0, 0);
             Title = "Menü";
             var syncButton = new CustomButton
             {
-                Text = "Sync \uf0e2",
-                
-                Margin = new Thickness(0,20)
+                Text = "Sync \uf0e2"
             };
-
-            //DetailListView.RowHeight = 40; 
-
             DetailListView.ItemTemplate = new DataTemplate(typeof(MenuTemplate));
 
             syncButton.ButtonLabel.FontSize = 40;
             syncButton.ButtonLabel.FontFamily = "FontAwesome";
             BindToViewModel(syncButton, CustomButton.CommandProperty, o => o.SyncCommand);
-            MasterContent = new Grid()
-            {
+             
+            _layoutGrid = new Grid
+            {  
                 RowDefinitions =
                 {
-                    new RowDefinition {Height = new GridLength(8, GridUnitType.Star)},
-                    new RowDefinition {Height = new GridLength(1, GridUnitType.Star)} 
+                    new RowDefinition { Height = new GridLength(7, GridUnitType.Star)},
+                    new RowDefinition { Height = new GridLength(2, GridUnitType.Star)} 
                 },
                 Children =
                 { 
                     DetailListView,
-                    {syncButton, 0, 1},
+                    { syncButton, 0, 1}
                 }
             };
-        } 
+
+            MasterContent = _layoutGrid;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); 
+            DetailListView.RowHeight =  (int) ( (Height/7.0*5) / ((MvvmNanoApplication)Application.Current).MasterDetails.Count);
+        }
     }
 }
