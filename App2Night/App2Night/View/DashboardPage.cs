3be9a07af046e550a6d355313759425621c7d782
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-11-25 10:36:09 +0100

    Refactored Dashboard and bind UserInfo.

--- App2Night/App2Night/View/DashboardPage.cs
+++ App2Night/App2Night/View/DashboardPage.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using App2Night.CustomView.Page;
 using App2Night.CustomView.Template;
 using App2Night.CustomView.View;
 using App2Night.Data.Language;
 using App2Night.Model.Model;
 using App2Night.ValueConverter;
-using App2Night.ViewModel; 
+using App2Night.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
@@ -17,41 +17,72 @@ namespace App2Night.View
     public class DashboardPage : ContentPageWithPreview<DashboardViewModel>
     {
         #region Views
-        Map _headerMap = new Map()
+
+        readonly EnhancedContainer _userInfoContainer = new EnhancedContainer
+        {
+            Name = AppResources.User,
+            ButtonText = "\uf0ad",
+            NoContentWarningVisible = false,
+            TopSpacerVisible = false
+        };
+
+        readonly EnhancedContainer _interestingPartieContainer = new EnhancedContainer
+        {
+            Name = AppResources.IntParty,
+            ContentMissingText = AppResources.ContIntParty
+        };
+
+        readonly EnhancedContainer _myPartiesContainer = new EnhancedContainer
+        {
+            Name = AppResources.MyParty,
+            ContentMissingText = AppResources.ContMyParty
+        };
+
+        readonly EnhancedContainer _historyContainer = new EnhancedContainer
+        {
+            Name = AppResources.History,
+            ContentMissingText = AppResources.ContHistory
+        };
+
+        readonly Map _headerMap = new Map()
         {
             HeightRequest = 200
         };
 
-        Image profilePicture = new Image 
+        readonly Image _profilePicture = new Image
         {
             HeightRequest = 100,
             WidthRequest = 100,
             Margin = new Thickness(10),
             Source = ImageSource.FromResource("App2Night.Data.IconCode.icon.png")
-        }; 
+        };
 
-        private HorizontalGallerieView historyGallerieView = new HorizontalGallerieView
+        readonly Label _usernameLabel = new Label();
+        readonly Label _emaiLabel = new Label();
+
+        readonly HorizontalGallerieView _historyGallerieView = new HorizontalGallerieView
         {
             Columns = 2,
-            Rows = 1, 
+            Rows = 1,
             Template = typeof(QuadraticPartyTemplate)
         };
 
-        private HorizontalGallerieView interestingPartieGallerie = new HorizontalGallerieView
+        readonly HorizontalGallerieView _interestingPartieGallerie = new HorizontalGallerieView
         {
             Columns = 1,
             Template = typeof(QuadraticPartyTemplate)
-        }; 
+        };
 
-        private HorizontalGallerieView myPartieGallerie = new HorizontalGallerieView
+        readonly HorizontalGallerieView _myPartieGallerie = new HorizontalGallerieView
         {
             Columns = 1,
             Template = typeof(QuadraticPartyTemplate)
         };
         #endregion
+
         public DashboardPage()
-        {   
-            //User info view
+        {
+            #region UserInfoView
             var userInfoView = new Grid
             {
                 ColumnDefinitions =
@@ -61,16 +92,16 @@ namespace App2Night.View
                 },
                 Children =
                 {
-                    profilePicture 
-                } 
-                
+                    _profilePicture
+                }
             };
-            var usernameLabel = new Label();
-            var joinedAtLabel = new Label() {Text = "Dabei seit: 30.10.2016"};
+            _userInfoContainer.Content = userInfoView;
+            BindToViewModel(_usernameLabel, Label.TextProperty, vm => vm.User.Name);
+            BindToViewModel(_emaiLabel, Label.TextProperty, vm => vm.User.Email);
             var profileDetails = new List<Xamarin.Forms.View>
             {
-                usernameLabel,
-                joinedAtLabel
+                _usernameLabel,
+                _emaiLabel
             };
             userInfoView.RowDefinitions.Add(new RowDefinition {Height = new GridLength(1, GridUnitType.Star)});
             for (int index = 0; index < profileDetails.Count; index++)
@@ -80,108 +111,98 @@ namespace App2Night.View
                 userInfoView.Children.Add(view, 1, index + 1);
             }
             userInfoView.RowDefinitions.Add(new RowDefinition {Height = new GridLength(1, GridUnitType.Star)});
-            Grid.SetRowSpan(profilePicture, profileDetails.Count + 2);
+            Grid.SetRowSpan(_profilePicture, profileDetails.Count + 2);
+            BindToViewModel(_userInfoContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToUserEditCommand);
+            #endregion
 
-            EnhancedContainer userInfoContainer = new EnhancedContainer
-            {
-				Name = AppResources.User,
-                ButtonText = "\uf0ad",
-                Content = userInfoView,
-                NoContentWarningVisible = false,
-                TopSpacerVisible = false
-            }; 
-            BindToViewModel(userInfoContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToUserEditCommand);
+            #region InterestingPartieView
+            _interestingPartieContainer.Content = _interestingPartieGallerie;
+            _interestingPartieGallerie.ElementTapped += PartieSelected;
+            BindToViewModel(_interestingPartieGallerie, GallerieView.ItemSourceProperty,
+                vm => vm.InterestingPartiesForUser);
+            BindToViewModel(_interestingPartieContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToPartyPicker);
+            BindToViewModel(_interestingPartieContainer, EnhancedContainer.NoContentWarnignVisibleProperty,
+                vm => vm.InterestingPartieAvailable, converter: new InvertBooleanConverter());
+            #endregion
 
-            //Interesting partie view
-            var interestingPartieContainer = new EnhancedContainer
-            {
-				Name = AppResources.IntParty, 
-                Content = interestingPartieGallerie,
-				ContentMissingText = AppResources.ContIntParty
-            };
-            interestingPartieGallerie.ElementTapped += PartieSelected;
-            BindToViewModel(interestingPartieGallerie, GallerieView.ItemSourceProperty, vm => vm.InterestingPartiesForUser);
-            BindToViewModel(interestingPartieContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToPartyPicker);
-            BindToViewModel(interestingPartieContainer, EnhancedContainer.NoContentWarnignVisibleProperty, vm => vm.InterestingPartieAvailable, converter: new InvertBooleanConverter());
+            #region MyParties
+            _myPartiesContainer.Content = _myPartieGallerie;
+            _myPartieGallerie.ElementTapped += PartieSelected;
+            BindToViewModel(_myPartiesContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToMyPartiesCommand);
+            BindToViewModel(_myPartieGallerie, GallerieView.ItemSourceProperty, vm => vm.Selectedparties);
+            BindToViewModel(_myPartiesContainer, EnhancedContainer.NoContentWarnignVisibleProperty,
+                vm => vm.SelectedpartiesAvailable, converter: new InvertBooleanConverter());
+            #endregion
 
-            //Users parties view
-            var myPartiesContainer = new EnhancedContainer
-            {
-				Name = AppResources.MyParty,
-                Content = myPartieGallerie,
-				ContentMissingText = AppResources.ContMyParty
-            };
-            myPartieGallerie.ElementTapped +=PartieSelected;
-            BindToViewModel(myPartiesContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToMyPartiesCommand);
-            BindToViewModel(myPartieGallerie, GallerieView.ItemSourceProperty, vm => vm.Selectedparties);
-            BindToViewModel(myPartiesContainer, EnhancedContainer.NoContentWarnignVisibleProperty, vm => vm.SelectedpartiesAvailable, converter: new InvertBooleanConverter());
-
-            //Partie history
-            var historyContainer = new EnhancedContainer
-            {
-				Name = AppResources.History, 
-                Content = historyGallerieView,
-				ContentMissingText = AppResources.ContHistory
-            };
-            BindToViewModel(historyContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToHistoryCommand);
-            historyGallerieView.ElementTapped += PartieSelected;
-            BindToViewModel(historyGallerieView, GallerieView.ItemSourceProperty, vm => vm.PartyHistory);
-            BindToViewModel(historyContainer, EnhancedContainer.NoContentWarnignVisibleProperty, vm => vm.PartyHistoryAvailable, converter: new InvertBooleanConverter());
-
+            #region History
+            _historyContainer.Content = _historyGallerieView;
+            BindToViewModel(_historyContainer, EnhancedContainer.CommandProperty, vm => vm.MoveToHistoryCommand);
+            _historyGallerieView.ElementTapped += PartieSelected;
+            BindToViewModel(_historyGallerieView, GallerieView.ItemSourceProperty, vm => vm.PartyHistory);
+            BindToViewModel(_historyContainer, EnhancedContainer.NoContentWarnignVisibleProperty,
+                vm => vm.PartyHistoryAvailable, converter: new InvertBooleanConverter());
+            #endregion
             //Main layout
             var mainLayout = new Grid()
-            {  
+            {
                 RowSpacing = 0,
                 RowDefinitions =
                 {
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Auto)},
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Auto)},
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Auto)},
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Auto)},
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Auto)},
-
+                    new RowDefinition {Height = new GridLength(1, GridUnitType.Auto)},
+                    new RowDefinition {Height = new GridLength(1, GridUnitType.Auto)},
+                    new RowDefinition {Height = new GridLength(1, GridUnitType.Auto)},
+                    new RowDefinition {Height = new GridLength(1, GridUnitType.Auto)},
+                    new RowDefinition {Height = new GridLength(1, GridUnitType.Auto)},
                 },
                 Children =
                 {
                     new MapWrapper(_headerMap),
-                    {userInfoContainer, 0, 1},
-                    { myPartiesContainer,0, 2},
-                    { interestingPartieContainer,0,3},
-                    { historyContainer,0, 4}
-                } 
+                    {_userInfoContainer, 0, 1},
+                    {_myPartiesContainer, 0, 2},
+                    {_interestingPartieContainer, 0, 3},
+                    {_historyContainer, 0, 4}
+                }
             };
 
             var mainScroll = new ScrollView
             {
                 Content = mainLayout,
                 Orientation = ScrollOrientation.Vertical
-            }; 
+            };
             Content = mainScroll;
         }
-
+        /// <summary>
+        /// Opens seleted party with <see cref="PartyPreviewView"/>.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="o"></param>
         private void PartieSelected(object sender, object o)
         {
             PreviewItemSelected<Party, PartyPreviewView>((Party) o, new object[] {Width, Height});
         }
 
         private int lastColumns = 1;
+        /// <summary>
+        /// Allocates size for <see cref="EnhancedContainer"/>. 
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
 
             //Handle big screens
-            int columns =  (int) Math.Ceiling((Width-200)/300); //Available columns
+            int columns = (int) Math.Ceiling((Width - 200)/300); //Available columns
             if (lastColumns != columns)
             {
                 lastColumns = columns;
-                historyGallerieView.Columns = columns;
-                interestingPartieGallerie.Columns = columns;
-                myPartieGallerie.Columns = columns;
+                _historyGallerieView.Columns = columns;
+                _interestingPartieGallerie.Columns = columns;
+                _myPartieGallerie.Columns = columns;
             }
 
-            profilePicture.HeightRequest = Width/2 - 20;
-            profilePicture.WidthRequest = Width/2 - 20; 
+            _profilePicture.HeightRequest = Width/2 - 20;
+            _profilePicture.WidthRequest = Width/2 - 20;
         }
-
     }
 }
