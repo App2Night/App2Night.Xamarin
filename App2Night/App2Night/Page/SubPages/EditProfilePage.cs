6a8cbfcd313b57e617e5191d6c7ac5f2fc02070f
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-11-30 10:32:05 +0100

    Modify EditPartyPage.

--- App2Night/App2Night/Page/SubPages/EditProfilePage.cs
+++ App2Night/App2Night/Page/SubPages/EditProfilePage.cs
@@ -9,39 +9,102 @@ namespace App2Night.Page.SubPages
 {
     public class EditProfilePage : FreshBaseContentPage
     {
-		#region Nodes
-		readonly InputContainer<Entry> _nameEntry = new InputContainer<Entry> { Input = { Placeholder = AppResources.Username }, IconCode ="\uf128", ValidationVisible = true };
+        #region Nodes
 
-		readonly InputContainer<Entry> _emailEntry = new InputContainer<Entry>{ Input = { Placeholder = AppResources.EmailAdress}, IconCode = "\uf003", ValidationVisible = true };
+        readonly InputContainer<Entry> _nameEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.Username},
+            IconCode = "\uf128",
+            ValidationVisible = true
+        };
 
-		readonly InputContainer<Entry> _addressEntry = new InputContainer<Entry>{ Input = { Placeholder = AppResources.Address }, IconCode = "\uf1ae", ValidationVisible = true };
+        readonly InputContainer<Entry> _emailEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.EmailAdress},
+            IconCode = "\uf003",
+            ValidationVisible = true
+        };
 
-		readonly InputContainer<Entry> _ageEntry = new InputContainer<Entry> { Input = { Placeholder = AppResources.Age, Keyboard = Keyboard.Numeric }, IconCode = "\uf1ae", ValidationVisible = true };
+        readonly InputContainer<Entry> _addressEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.Address},
+            IconCode = "\uf1ae",
+            ValidationVisible = true
+        };
 
-		readonly InputContainer<EnumBindablePicker<Gender>> _genderPicker = new InputContainer<EnumBindablePicker<Gender>>{ IconCode = "\uf183", ValidationVisible = true };
+        readonly InputContainer<Entry> _ageEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.Age, Keyboard = Keyboard.Numeric},
+            IconCode = "\uf1ae",
+            ValidationVisible = true
+        };
 
-		// TODO handle IconCode
-		Image _image = new Image { BackgroundColor = Color.Gray, HeightRequest = 300};
+        readonly InputContainer<EnumBindablePicker<Gender>> _genderPicker =
+            new InputContainer<EnumBindablePicker<Gender>> {IconCode = "\uf183", ValidationVisible = true};
 
-		private readonly CustomButton _cancelBtn = new CustomButton
+        // TODO handle IconCode
+        Image _image = new Image {BackgroundColor = Color.Gray, HeightRequest = 300};
+
+        private readonly CustomButton _cancelBtn = new CustomButton
         {
             Text = "\uf00d",
-            ButtonLabel = { FontFamily = "FontAwesome", FontSize = 50 },
+            ButtonLabel = {FontFamily = "FontAwesome", FontSize = 50},
         };
 
         private readonly CustomButton _okBtn = new CustomButton
         {
             Text = "\uf00c",
-            ButtonLabel = { FontFamily = "FontAwesome", FontSize = 50 },
+            ButtonLabel = {FontFamily = "FontAwesome", FontSize = 50},
         };
-		#endregion
+
+        private InputContainer<Entry> _cityNameEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.Cityname},
+            IconCode = "\uf279",
+            ValidationVisible = true
+
+        };
+
+        private InputContainer<Entry> _streetEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.StrName},
+            ValidationVisible = true,
+
+            IconCode = "\uf0f3"
+        };
+
+        private InputContainer<Entry> _numberEntry = new InputContainer<Entry>
+        {
+            Input = {Keyboard = Keyboard.Numeric, Placeholder = AppResources.HNumber},
+            ValidationVisible = true
+
+        };
+
+        private InputContainer<Entry> _locationEntry = new InputContainer<Entry>
+        {
+            Input = {Placeholder = AppResources.Location},
+            IconCode = "\uf015",
+            ValidationVisible = true,
+
+            Margin = new Thickness(5, 0)
+        };
+
+        InputContainer<Entry> _zipCodetEntry = new InputContainer<Entry>
+        {
+            Input = {Keyboard = Keyboard.Numeric, Placeholder = AppResources.Zipcode},
+            ValidationVisible = true
+
+        };
+
+        #endregion
+
         /// <summary>
         /// Page where Name, Email, Address etc. can be changed.
         /// </summary>
         public EditProfilePage()
         {
             // set title and add Command for ViewModel
-			Title = AppResources.EditProfile;
+            Title = AppResources.EditProfile;
             // bind to view model
             _cancelBtn.SetBinding(CustomButton.CommandProperty, "MoveToCancelCommand");
             _okBtn.SetBinding(CustomButton.CommandProperty, "MoveTOkCommand");
@@ -54,23 +117,25 @@ namespace App2Night.Page.SubPages
 
             _genderPicker.Input.SetBinding(EnumBindablePicker<Gender>.SelectedItemProperty, "User.Gender");
 
-			// set event handler
-			_okBtn.ButtonTapped += OnOkBtnTapped;
+            // set event handler
+            _okBtn.ButtonTapped += OnOkBtnTapped;
             // set Content with two grids. first one contains all information about the user. last one has a cancel and ok btn.
-			StackLayout stackLayout = new StackLayout()
+            StackLayout stackLayout = new StackLayout()
             {
                 Children =
                 {
-					_image,
-					_nameEntry,
-					_emailEntry,
-					_ageEntry,
-					_addressEntry,
-					_genderPicker,
+                    _nameEntry,
+                    _emailEntry,
+                    _ageEntry,
+                    _genderPicker,
+                    _streetEntry,
+                    _numberEntry,
+                    _cityNameEntry,
+                    _zipCodetEntry,
                     new BoxView
                     {
-                       HeightRequest = 1,
-                       BackgroundColor = Color.Black
+                        HeightRequest = 1,
+                        BackgroundColor = Color.Black
                     },
                     new Grid
                     {
@@ -87,58 +152,58 @@ namespace App2Night.Page.SubPages
                     }
                 }
             };
-			var mainScroll = new ScrollView
-			{
-				Content = stackLayout,
-				Orientation = ScrollOrientation.Vertical
-			};
-			Content = mainScroll;
+            var mainScroll = new ScrollView
+            {
+                Content = stackLayout,
+                Orientation = ScrollOrientation.Horizontal
+                //{
+
+                //    new Grid
+                //    {
+                //        RowDefinitions = new RowDefinitionCollection
+                //        {
+                //            new RowDefinition {Height = new GridLength(1, GridUnitType.Star)},
+                //            new RowDefinition {Height = new GridLength(1, GridUnitType.Star)},
+                //        },
+                //        Children =
+                //        {
+                //            {_image, 0, 0},
+                //            {stackLayout, 1, 0}
+                //        }
+                //    }
+
+                //},
+                
+            };
+            Content = stackLayout;
         }
 
         private void OnOkBtnTapped(object sender, EventArgs e)
         {
-			if (_okBtn.Command != null)
-			{
-				var animation = new Animation(d =>
-				{
-					_okBtn.Scale = d;
-				}, 1, 1.6);
-				var nextAnimation = new Animation(d =>
-				{
-					_okBtn.Scale = d;
-				}, 1.6, 1);
-				animation.Commit(this, "Scale", finished: delegate
-				{
-					nextAnimation.Commit(this, "Descale");
-				});
-				_okBtn.Command.Execute(null);
-			}
+            if (_okBtn.Command != null)
+            {
+                var animation = new Animation(d => { _okBtn.Scale = d; }, 1, 1.6);
+                var nextAnimation = new Animation(d => { _okBtn.Scale = d; }, 1.6, 1);
+                animation.Commit(this, "Scale", finished: delegate { nextAnimation.Commit(this, "Descale"); });
+                _okBtn.Command.Execute(null);
+            }
         }
 
-		private void OnCancelBtnTapped(object sender, EventArgs e)
-		{
-			if (_cancelBtn.Command != null)
-			{
-				var animation = new Animation(d =>
-				{
-					_cancelBtn.Scale = d;
-				}, 1, 1.6);
-				var nextAnimation = new Animation(d =>
-				{
-					_cancelBtn.Scale = d;
-				}, 1.6, 1);
-				animation.Commit(this, "Scale", finished: delegate
-				{
-					nextAnimation.Commit(this, "Descale");
-				});
-				_cancelBtn.Command.Execute(null);
-			}
-		}
+        private void OnCancelBtnTapped(object sender, EventArgs e)
+        {
+            if (_cancelBtn.Command != null)
+            {
+                var animation = new Animation(d => { _cancelBtn.Scale = d; }, 1, 1.6);
+                var nextAnimation = new Animation(d => { _cancelBtn.Scale = d; }, 1.6, 1);
+                animation.Commit(this, "Scale", finished: delegate { nextAnimation.Commit(this, "Descale"); });
+                _cancelBtn.Command.Execute(null);
+            }
+        }
 
         protected override void OnDisappearing()
         {
-            _okBtn.ButtonTapped -= OnOkBtnTapped;
             base.OnDisappearing();
-        } 
+            _okBtn.ButtonTapped -= OnOkBtnTapped;
+        }
     }
 }
