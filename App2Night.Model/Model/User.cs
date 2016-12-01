ac49f092337b7550fda9160b1d757d5593ea51d6
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-12-01 14:33:56 +0100

    Remove Gender, Age in Model.

--- App2Night.Model/Model/User.cs
+++ App2Night.Model/Model/User.cs
@@ -7,8 +7,6 @@ namespace App2Night.Model.Model
     public class User 
     {
         public string Name { get; set; }
-        public int Age { get; set; }
-		public Gender Gender { get; set; } = Gender.Unkown;
         public string Email { get; set; }
         public ObservableCollection<Party> Events { get; set; }
         public Location Addresse { get; set; }
