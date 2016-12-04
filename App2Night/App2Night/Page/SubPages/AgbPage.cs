dd66d53e9cf63bd668b1953bc75f561b9c086a95
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-12-04 22:40:08 +0100

    Add AGB Page.

---
+++ App2Night/App2Night/Page/SubPages/AgbPage.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection;
+using App2Night.Data.Language;
+using FreshMvvm;
+using Xamarin.Forms;
+
+namespace App2Night.Page.SubPages
+{
+    public class AgbPage : FreshBaseContentPage
+    {
+        public AgbPage()
+        {
+            Title = AppResources.AgbHeader;
+
+            var assembly = typeof(AgbPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("App2Night.Data.Licenses.AGB.txt");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            Content = new ScrollView {Content = new Label {Text = text} , Padding = new Thickness(5)};
+        }
+    }
+}
