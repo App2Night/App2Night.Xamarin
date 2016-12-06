cfeb04df0bc6efc9c5d847cd23e289218c93dffc
Konrad Mueller <i15019@hb.dhbw-stuttgart.de>
2016-12-06 20:59:36 +0100

    Added selected parties page.

--- App2Night/App2Night/Page/SubPages/MyPartysPage.cs
+++ App2Night/App2Night/Page/SubPages/MyPartysPage.cs
@@ -1,9 +1,30 @@
-﻿using FreshMvvm;
+﻿using App2Night.CustomView.Page;
+using App2Night.CustomView.Template;
+using FreshMvvm;
+using Xamarin.Forms;
 
 namespace App2Night.Page.SubPages
 {
-    public class MyPartysPage : FreshBaseContentPage 
+    public class MyPartysPage : CustomContentPage 
     {
-        
+        public MyPartysPage()
+        {
+            NoContentWarningMessage = "You did not note, accept or create a party yet."; 
+
+            var listView = new ListView
+            {
+               IsPullToRefreshEnabled = true,
+               ItemTemplate = new DataTemplate(typeof(PartyTemplate))
+            };
+
+            listView.SetBinding(ListView.ItemsSourceProperty, "MyParties");
+            listView.SetBinding(ListView.SelectedItemProperty, "SelectedParty");
+            listView.SetBinding(ListView.RefreshCommandProperty, "RefreshCommand");
+            listView.SetBinding(ListView.IsRefreshingProperty, "IsRefreshing");
+
+            this.SetBinding(CustomContentPage.ShowNoContentWarningProperty, "NoContentAvailable");
+
+            Content = listView;
+        }
     }
 }
