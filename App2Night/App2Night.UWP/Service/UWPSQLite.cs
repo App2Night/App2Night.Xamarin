3961139c1fd21ae91b2bd35ec9b0fca82d76cb35
Konrad Müller <k.l.mueller@outlook.com>
2016-12-11 01:34:13 +0100

    Added SQLight to UWP.

---
+++ App2Night/App2Night.UWP/Service/UWPSQLite.cs
@@ -0,0 +1,36 @@
+﻿ 
+
+
+using System;
+using System.IO;
+using Windows.Storage;
+using App2Night.Service.Interface;
+using App2Night.UWP.Service;
+using SQLite.Net;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(UWPSQLite))]
+namespace App2Night.UWP.Service
+{
+    public class UWPSQLite : IDatabaseService
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "App2NightSQLite.db3";
+            var folder = ApplicationData.Current.LocalCacheFolder; // Documents folder 
+            var path = Path.Combine(folder.Path, sqliteFilename);  
+
+            // This is where we copy in the prepopulated database 
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            var plat = new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT();
+            var conn = new SQLite.Net.SQLiteConnection(plat, path);
+
+            // Return the database connection 
+            return conn;
+        }
+    }
+}
